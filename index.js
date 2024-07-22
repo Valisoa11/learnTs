77d0d981d650aff88276775aee5b92b201a24366
Valisoa11 <iriantsoaaa@gmail.com>
2024-07-22 23:39:53 +0200

    files deleted

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const numbers = [1, 2, 3]; // inferred to type number[]
-numbers.push(4); // no error
-// comment line below out to see the successful assignment 
-console.log(numbers);
-let head = numbers[0]; // no error
-console.log(head);
